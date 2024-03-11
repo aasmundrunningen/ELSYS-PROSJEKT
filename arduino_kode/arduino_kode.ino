f3af77912d195e2bb6671629917113af55cd78b1
aasmundrunningen <43582680+aasmundrunningen@users.noreply.github.com>
2024-03-11 11:33:16 +0100

    arduinoen klarer å gå inn og ut av lavstrømsmodus.
    Må rydde opp i koden og lage flere seperate funksjoner for lavstrømsmodus

--- arduino_kode/arduino_kode.ino
+++ arduino_kode/arduino_kode.ino
@@ -1,37 +1,79 @@
 #include "ArduinoLowPower.h"
 
-#define battery_indicator A1
-#define battery_inidcator_switch 3
+#define BATTERY_INDICATOR_BUTTON 3
+#define MEASURE_MODE_SWITCH 4
+#define USB_DETECT 1
+#define GREEN_LED 8
+#define YELLOW_LED 7
+#define RED_LED 6
+
 
 #define measure_switch 9
 
-void begin(){
-  LowPower.attachInterruptWakeup(pin, callback, mode)
+enum State{
+  INIT,
+  MEASURE,
+  BATTERY_MEASURE,
+  CHARGING,
+  DEEP_SLEEP
+};
 
+enum State state = INIT;
+
+void setup(){
+  led_init();
+  digitalWrite(GREEN_LED, HIGH);
+  while(!Serial){
+    Serial.begin(9600);
+  }
+  
+  Serial.println("Starting program");
+  pinMode(USB_DETECT, INPUT);
+  pinMode(MEASURE_MODE_SWITCH, INPUT_PULLUP);
+  pinMode(BATTERY_INDICATOR_BUTTON, INPUT_PULLUP);
 }
 
 void loop(){
-
+  digitalWrite(GREEN_LED, HIGH);
+  delay(5000);
+  digitalWrite(GREEN_LED, LOW);
+  turn_on_low_power();
 }
 
 
+void led_init(){
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  digitalWrite(GREEN_LED, HIGH);
+  digitalWrite(YELLOW_LED, HIGH);
+  digitalWrite(RED_LED, HIGH);
+  delay(5000);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+}
+
 void turn_on_low_power(){
-  LowPower.attachInterruptWakeup(battery_indicator_switch, measure_battery_callback, FALLING);
-  LowPower.attachInterruptWakeup(measure_switch, measure_callback, FALLING);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, HIGH);
+  Serial.begin(9600);
+  if(Serial){
+    Serial.println("Going into deep sleep in 2s");
+  }
+  delay(2000);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(RED_LED, HIGH);
+  //LowPower.attachInterruptWakeup(BATTERY_INDICATOR_BUTTON, measure_battery_callback, FALLING);
+  LowPower.attachInterruptWakeup(MEASURE_MODE_SWITCH, measure_callback, FALLING);
   LowPower.deepSleep();
 }
 
-void measure_battery_init(){
-  pinMode(battery_indicator, INPUT);
-  pinMode(battery_indicator_switch, INPUT_PULLUP);
-}
-
-
-
 void measure_battery_callback(){
-
+  state = BATTERY_MEASURE
+  
 }
 
 void measure_callback(){
-
+  state = MEASURE;
 }
